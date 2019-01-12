89d09f3bf69eebf95eb5c03493e7420325e8fac2
yuzhenzero <yuzhenzero@hotmail.com>
2019-01-12 14:14:45 +0800

    add Solution and Note 328. Odd Even Linked List.

---
+++ src/com/leetcode/OddEvenLinkedList.java
@@ -0,0 +1,25 @@
+package com.leetcode;
+
+// 328#, Medium
+public class OddEvenLinkedList {
+    public ListNode oddEvenList (ListNode head) {
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+
+    public static void main (String[] args) {
+        OddEvenLinkedList oell = new OddEvenLinkedList();
+        ListNode head = ListNodeWrapper.stringToListNode("[1,2,3,4,5]");
+        head = oell.oddEvenList(head);
+        ListNodeWrapper.prettyPrintLinkedList(head);
+    }
+}
