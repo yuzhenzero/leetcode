daca976cafa17eae476c7b4e5e3e021af073134c
yuzhenzero <yuzhenzero@hotmail.com>
2019-01-16 10:53:23 +0800

    add 230.Kth Smallest Element in a BST and note.

---
+++ src/com/leetcode/KthSmallestElementinaBST.java
@@ -0,0 +1,25 @@
+package com.leetcode;
+
+// 230#, Medium
+public class KthSmallestElementinaBST {
+    public int kthSmallest (TreeNode root, int k) {
+        int t = size(root.left);
+        if (t > k - 1) return kthSmallest(root.left, k);
+        else if (t < k - 1) return kthSmallest(root.right, k - t - 1);
+        else return root.val;
+    }
+
+    private int size (TreeNode root) {
+        if (root == null) return 0;
+        return size(root.left) + size(root.right) + 1;
+    }
+
+    public static void main (String[] args) {
+        KthSmallestElementinaBST kseBST = new KthSmallestElementinaBST();
+        TreeNode root = TreeNodeWrapper.stringToTreeNode("[5,3,6,2,4,null,null,1]");
+        TreeNodeWrapper.prettyPrintTree(root);
+        int k = 3;
+        int ans = kseBST.kthSmallest(root, k);
+        System.out.println(ans);
+    }
+}
