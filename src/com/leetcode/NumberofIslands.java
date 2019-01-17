eaaaa9e1ab591766d8c8b4661164c6ec8cd0daf4
yuzhenzero <yuzhenzero@hotmail.com>
2019-01-17 11:06:12 +0800

    add 200.Number of Islands and note.

---
+++ src/com/leetcode/NumberofIslands.java
@@ -0,0 +1,40 @@
+package com.leetcode;
+
+// 200#, Medium
+public class NumberofIslands {
+    public int numIslands (char[][] grid) {
+        int m = grid.length;
+        if (m == 0) return 0;
+        int n = grid[0].length;
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void dfs (char[][] grid, int i, int j) {
+        if (i >= 0 && j >= 0 && i < grid.length && j < grid[0].length && grid[i][j] == '1') {
+            grid[i][j] = '0';
+            dfs(grid, i + 1, j);
+            dfs(grid, i - 1, j);
+            dfs(grid, i, j + 1);
+            dfs(grid, i, j - 1);
+        }
+    }
+
+    public static void main (String[] args) {
+        NumberofIslands ni = new NumberofIslands();
+        char[][] grid =
+                {{'1', '1', '1', '1', '0'},
+                        {'1', '1', '0', '1', '0'},
+                        {'1', '1', '0', '0', '0'},
+                        {'0', '0', '0', '0', '1'}};
+        System.out.println(ni.numIslands(grid));
+    }
+}
