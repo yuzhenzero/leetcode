f8b614f13237553df418c78e517a02df89bf8bcf
yuzhenzero <yuzhenzero@hotmail.com>
2019-04-15 16:20:37 +0800

    修改53. 最大子序和的错别字

--- src/com/leetcode/MaximumSubarray.java
+++ src/com/leetcode/MaximumSubarray.java
@@ -23,7 +23,7 @@ public class MaximumSubarray {
     // 定义一个数组，dp[]，dp[i]是**以第i个元素为结尾的一段最大子序和**。
     // 求dp[i]时，假设前面dp[0]~dp[i-1]都已经求出来了，dp[i-1]表示的是以i-1为结尾的最大子序和，
     // 若dp[i-1]小于0，则dp[i]加上前面的任意长度的序列和都会小于不加前面的序列
-    // （即自己本身一个元素是以自己为结尾的最大自序和）
+    // （即自己本身一个元素是以自己为结尾的最大子序和）
     public int maxSubArray_dp (int[] nums) {
         if (nums.length == 0) return 0;
         if (nums.length == 1) return nums[0];
