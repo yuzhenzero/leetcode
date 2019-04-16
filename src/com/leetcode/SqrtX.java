46d61d93b41f3326be7e02a5ab400d38166c2619
yuzhenzero <yuzhenzero@hotmail.com>
2019-04-16 09:52:42 +0800

    69. x 的平方根

---
+++ src/com/leetcode/SqrtX.java
@@ -0,0 +1,28 @@
+package com.leetcode;
+
+// 69#, Easy
+public class SqrtX {
+    public int mySqrt (int x) {
+        if (x <= 1)
+            return x;
+        int l = 0, h = x;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            int sqrt = x / mid;
+            if (mid == sqrt) {
+                return mid;
+            } else if (mid > sqrt) {
+                h = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return h;
+    }
+
+    public static void main (String[] args) {
+        SqrtX sx = new SqrtX();
+        int x = 8;
+        System.out.println(sx.mySqrt(x));
+    }
+}
