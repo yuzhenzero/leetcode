3ff5aca13dab7c45921ef6d1e6db40248ae9ae3c
yuzhenzero <yuzhenzero@hotmail.com>
2019-04-18 11:18:19 +0800

    修改 200. 岛屿的个数

--- src/com/leetcode/NumberofIslands.java
+++ src/com/leetcode/NumberofIslands.java
@@ -2,6 +2,7 @@ package com.leetcode;
 
 // 200#, Medium
 public class NumberofIslands {
+    private int[][] direction ={{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
     public int numIslands (char[][] grid) {
         int m = grid.length;
         if (m == 0) return 0;
@@ -21,10 +22,9 @@ public class NumberofIslands {
     private void dfs (char[][] grid, int i, int j) {
         if (i >= 0 && j >= 0 && i < grid.length && j < grid[0].length && grid[i][j] == '1') {
             grid[i][j] = '0';
-            dfs(grid, i + 1, j);
-            dfs(grid, i - 1, j);
-            dfs(grid, i, j + 1);
-            dfs(grid, i, j - 1);
+            for (int[] d : direction) {
+                dfs(grid, i + d[0], j + d[1]);
+            }
         }
     }
 
