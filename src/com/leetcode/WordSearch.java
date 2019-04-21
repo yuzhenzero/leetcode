ed216d4fa171aca3da1c9fcf5bf3c3f32a2176d9
yuzhenzero <yuzhenzero@hotmail.com>
2019-04-21 20:55:47 +0800

    更新 79. 单词搜索

--- src/com/leetcode/WordSearch.java
+++ src/com/leetcode/WordSearch.java
@@ -2,9 +2,17 @@ package com.leetcode;
 
 // 79#, Medium
 public class WordSearch {
+    private int m, n;
+
     public boolean exist (char[][] board, String word) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
+        if (board == null || board.length == 0) {
+            return false;
+        }
+        m = board.length;
+        n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (board[i][j] == word.charAt(0)) {
                     if (backtrack(i, j, 0, board, word)) {
                         return true;
@@ -16,25 +24,31 @@ public class WordSearch {
     }
 
     private boolean backtrack (int i, int j, int index, char[][] board, String word) {
-        if (word.length() == index) return true;
-        if (i < 0 || i >= board.length ||
-                j < 0 || j >= board[0].length ||
-                board[i][j] != word.charAt(index))
+        if (index == word.length()) {
+            return true;
+        }
+        if (i < 0 || i >= m || j < 0 || j >= n ||
+                board[i][j] != word.charAt(index)) {
             return false;
-        char c = board[i][j];
-        board[i][j] = '*'; // 标记，表示已使用过，不能再重复
+        }
+        char temp = board[i][j];
+        board[i][j] = '*';
         boolean ans;
-        ans = backtrack(i + 1, j, index + 1, board, word)
-                || backtrack(i - 1, j, index + 1, board, word)
-                || backtrack(i, j + 1, index + 1, board, word)
-                || backtrack(i, j - 1, index + 1, board, word);
-        board[i][j] = c; // 用完之后还要换回来，方便别的元素使用
+        ans = backtrack(i + 1, j, index + 1, board, word) ||
+                backtrack(i - 1, j, index + 1, board, word) ||
+                backtrack(i, j + 1, index + 1, board, word) ||
+                backtrack(i, j - 1, index + 1, board, word);
+        board[i][j] = temp;
         return ans;
     }
 
+
     public static void main (String[] args) {
         WordSearch ws = new WordSearch();
-        char[][] board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
+        char[][] board = {
+                {'A', 'B', 'C', 'E'},
+                {'S', 'F', 'C', 'S'},
+                {'A', 'D', 'E', 'E'}};
         String word = "ABCB";
         System.out.println(ws.exist(board, word));
     }
