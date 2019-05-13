77d65269ff6b63b0d9c0a54de3cafd8966990576
yuzhenzero <yuzhenzero@hotmail.com>
2019-05-13 17:39:50 +0800

    234. Palindrome Linked List

---
+++ src/com/leetcode/PalindromeLinkedList.java
@@ -0,0 +1,49 @@
+package com.leetcode;
+
+/**
+ * @author Yu Zhen
+ **/
+//234#, Easy
+public class PalindromeLinkedList {
+    public boolean isPalindrome (ListNode head) {
+        if (head == null || head.next == null) return true;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        slow = slow.next;
+        slow = reverse(slow);
+
+        while (slow != null) {
+            if (head.val == slow.val) {
+                head = head.next;
+                slow = slow.next;
+            } else
+                return false;
+        }
+        return true;
+
+    }
+
+    private ListNode reverse (ListNode head) {
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode nextNode = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = nextNode;
+        }
+        return newHead;
+    }
+
+
+    public static void main (String[] args) {
+        PalindromeLinkedList pll = new PalindromeLinkedList();
+        ListNode head = ListNodeWrapper.stringToListNode("[1,2,3,2,1]");
+        System.out.println(pll.isPalindrome(head));
+    }
+}
