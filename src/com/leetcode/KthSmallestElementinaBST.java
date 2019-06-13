83f8a30ba3b87cbd522788bbd188ac827c423bc9
yuzhenzero <yuzhenzero@hotmail.com>
2019-06-13 10:02:14 +0800

    230. 二叉搜索树中第K小的元素 增加中序遍历法解法

--- src/com/leetcode/KthSmallestElementinaBST.java
+++ src/com/leetcode/KthSmallestElementinaBST.java
@@ -5,6 +5,10 @@ import com.leetcode.repository.TreeNodeWrapper;
 
 // 230#, Medium
 public class KthSmallestElementinaBST {
+    int cnt = 0;
+    int val;
+
+    // 计算节点数法
     public int kthSmallest (TreeNode root, int k) {
         int t = size(root.left);
         if (t > k - 1) return kthSmallest(root.left, k);
@@ -17,12 +21,31 @@ public class KthSmallestElementinaBST {
         return size(root.left) + size(root.right) + 1;
     }
 
+    // 中序遍历法
+    public int kthSmallestInorder (TreeNode root, int k) {
+        inorder(root, k);
+        return val;
+    }
+
+    private void inorder (TreeNode root, int k) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, k);
+        cnt++;
+        if (cnt == k) {
+            val = root.val;
+            return;
+        }
+        inorder(root.right, k);
+    }
+
     public static void main (String[] args) {
         KthSmallestElementinaBST kseBST = new KthSmallestElementinaBST();
         TreeNode root = TreeNodeWrapper.stringToTreeNode("[5,3,6,2,4,null,null,1]");
         TreeNodeWrapper.prettyPrintTree(root);
         int k = 3;
-        int ans = kseBST.kthSmallest(root, k);
+        int ans = kseBST.kthSmallestInorder(root, k);
         System.out.println(ans);
     }
 }
