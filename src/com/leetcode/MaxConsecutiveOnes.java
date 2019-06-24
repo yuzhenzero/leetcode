1816d0ec6296093c19134842e39734ee3554abd6
yuzhenzero <yuzhenzero@hotmail.com>
2019-06-24 10:13:48 +0800

    485. 最大连续1的个数 增加一种解法

--- src/com/leetcode/MaxConsecutiveOnes.java
+++ src/com/leetcode/MaxConsecutiveOnes.java
@@ -1,20 +1,17 @@
 package com.leetcode;
+
 // 485#, Easy
 public class MaxConsecutiveOnes {
-    public int findMaxConsecutiveOnes(int[] nums)
-    {
+    public int findMaxConsecutiveOnes (int[] nums) {
         int max_num = 0;
 
         int max_temp = 0;
-        for (int i = 0; i < nums.length; i++)
-        {
+        for (int i = 0; i < nums.length; i++) {
 
-            if (nums[i] == 1)
-            {
+            if (nums[i] == 1) {
                 max_temp++;
             }
-            if (nums[i] == 0)
-            {
+            if (nums[i] == 0) {
                 max_temp = 0;
             }
 
@@ -26,10 +23,18 @@ public class MaxConsecutiveOnes {
         return max_num;
     }
 
-    public static void main(String[] args)
-    {
+    public int findMaxConsecutiveOnes2 (int[] nums) {
+        int max = 0, count = 0;
+        for (int num : nums) {
+            count = num == 1 ? count + 1 : 0;
+            max = Math.max(count, max);
+        }
+        return max;
+    }
+
+    public static void main (String[] args) {
         MaxConsecutiveOnes mco = new MaxConsecutiveOnes();
-        int[] a = {1,1,0,1,1,1};
-        System.out.println(mco.findMaxConsecutiveOnes(a));
+        int[] a = {1, 1, 0, 1, 1, 1};
+        System.out.println(mco.findMaxConsecutiveOnes2(a));
     }
 }
