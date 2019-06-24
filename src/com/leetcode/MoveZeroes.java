cd7e710d2c17871dbca9d50b642749ddb2a78373
yuzhenzero <yuzhenzero@hotmail.com>
2019-06-24 09:18:01 +0800

    283. 移动零 增加一种解法

--- src/com/leetcode/MoveZeroes.java
+++ src/com/leetcode/MoveZeroes.java
@@ -17,11 +17,26 @@ public class MoveZeroes {
         }
     }
 
+    public void moveZeroes2 (int[] nums) {
+
+        int count = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                nums[count++] = num;
+
+            }
+        }
+
+        for (int i = count; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
     public static void main(String[] args)
     {
         MoveZeroes mz = new MoveZeroes();
         int[] a = {0,1,0,3,12};
-        mz.moveZeroes(a);
+        mz.moveZeroes2(a);
         System.out.println(Arrays.toString(a));
     }
 }
