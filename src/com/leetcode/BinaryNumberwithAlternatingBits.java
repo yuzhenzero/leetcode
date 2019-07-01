e73381b87e36c6cb606e0212b00d1596ff74fc08
yuzhenzero <yuzhenzero@hotmail.com>
2019-07-01 17:08:19 +0800

    693. 交替位二进制数

---
+++ src/com/leetcode/BinaryNumberwithAlternatingBits.java
@@ -0,0 +1,18 @@
+package com.leetcode;
+
+/**
+ * 2019-07-01 16:52
+ *
+ * @author Yu Zhen
+ **/
+public class BinaryNumberwithAlternatingBits {
+    public boolean hasAlternatingBits (int n) {
+        int tmp = (n ^ (n >> 1));
+        return (tmp & (tmp + 1)) == 0;
+    }
+
+    public static void main (String[] args) {
+        BinaryNumberwithAlternatingBits test = new BinaryNumberwithAlternatingBits();
+        System.out.println(test.hasAlternatingBits(5));
+    }
+}
