87b3b29501d97f03799b6c47283d3be4c9c9354d
yuzhenzero <yuzhenzero@hotmail.com>
2019-07-02 16:58:59 +0800

    64. 最小路径和

--- src/com/leetcode/UniquePaths.java
+++ src/com/leetcode/UniquePaths.java
@@ -1,5 +1,7 @@
 package com.leetcode;
 
+import java.util.Arrays;
+
 // 66#, Medium
 public class UniquePaths {
     public int uniquePaths (int m, int n) {
@@ -19,9 +21,20 @@ public class UniquePaths {
         return matrix[m - 1][n - 1];
     }
 
+    public int uniquePaths2 (int m, int n) {
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[j] = dp[j] + dp[j - 1];
+            }
+        }
+        return dp[n - 1];
+    }
+
     public static void main (String[] args) {
         UniquePaths up = new UniquePaths();
         int m = 7, n = 3;
-        System.out.println(up.uniquePaths(m, n));
+        System.out.println(up.uniquePaths2(m, n));
     }
 }
