7b06494e316e7acb28f160d11de403916efcd544
yuzhenzero <yuzhenzero@hotmail.com>
2019-07-02 10:27:02 +0800

    198. 打家劫舍 增加一种解法

--- src/com/leetcode/HouseRobber.java
+++ src/com/leetcode/HouseRobber.java
@@ -13,11 +13,21 @@ public class HouseRobber {
         return dp[nums.length];
     }
 
+    public int rob2 (int[] nums) {
+        int pre1 = 0, pre2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int cur = Math.max(pre1, pre2 + nums[i]);
+            pre2 = pre1;
+            pre1 = cur;
+        }
+        return pre1;
+    }
+
     public static void main (String[] args) {
         HouseRobber hr = new HouseRobber();
         int[] nums1 = {2,7,9,3,1};
         int[] nums2 = {1,2,3,1};
-        System.out.println(hr.rob(nums1));
-        System.out.println(hr.rob(nums2));
+        System.out.println(hr.rob2(nums1));
+        System.out.println(hr.rob2(nums2));
     }
 }
