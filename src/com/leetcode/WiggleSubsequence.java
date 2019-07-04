c0ed069eaa056299d8c56148071d852bcda4d8e7
yuzhenzero <yuzhenzero@hotmail.com>
2019-07-04 17:24:09 +0800

    376. 摆动序列

---
+++ src/com/leetcode/WiggleSubsequence.java
@@ -0,0 +1,30 @@
+package com.leetcode;
+
+/**
+ * 2019-07-04 17:12
+ * 376, Medium
+ *
+ * @author Yu Zhen
+ **/
+public class WiggleSubsequence {
+    public int wiggleMaxLength (int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int up = 1, down = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                up = down + 1;
+            } else if (nums[i] < nums[i - 1]) {
+                down = up + 1;
+            }
+        }
+        return Math.max(up, down);
+    }
+
+    public static void main (String[] args) {
+        WiggleSubsequence test = new WiggleSubsequence();
+        int[] nums = {1, 17, 5, 10, 13, 15, 10, 5, 16, 8};
+        System.out.println(test.wiggleMaxLength(nums));
+    }
+}
