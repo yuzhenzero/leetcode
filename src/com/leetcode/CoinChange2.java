66892fd9e8198667f3d86b3eda90cac7a44fe45c
yuzhenzero <yuzhenzero@hotmail.com>
2019-07-23 20:03:02 +0800

    518. 零钱兑换 II 修改边界条件

--- src/com/leetcode/CoinChange2.java
+++ src/com/leetcode/CoinChange2.java
@@ -8,8 +8,11 @@ package com.leetcode;
  **/
 public class CoinChange2 {
     public int change (int amount, int[] coins) {
-        if (amount == 0 || coins == null || coins.length == 0) {
+        if (amount < 0 || (coins.length == 0 && amount != 0)) {
             return 0;
+        }
+        if (coins.length == 0 || amount == 0) {
+            return 1;
         }
         int[] dp = new int[amount + 1];
         dp[0] = 1;
