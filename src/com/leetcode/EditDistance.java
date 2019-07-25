1cd0d5ea8e64e22914db5655bd159ed1b73f6948
yuzhenzero <yuzhenzero@hotmail.com>
2019-07-25 11:50:32 +0800

    72. 编辑距离

---
+++ src/com/leetcode/EditDistance.java
@@ -0,0 +1,37 @@
+package com.leetcode;
+
+/**
+ * 2019-07-25 11:15
+ * 72, Hard
+ *
+ * @author Yu Zhen
+ **/
+public class EditDistance {
+    public int minDistance (String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i <= n; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    public static void main (String[] args) {
+        EditDistance test = new EditDistance();
+        String word1 = "horse";
+        String word2 = "ros";
+        System.out.println(test.minDistance(word1, word2));
+    }
+}
